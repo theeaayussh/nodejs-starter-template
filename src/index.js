ba932c88d4769a2ea9486a9c4b789981eea470d3
theeaayussh <aayushj9173@gmail.com>
2025-10-06 04:23:35 +0530

    add logger service

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { PORT } = require("./config");
+const { ServerConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
 
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server Started Successfully on Port : ${PORT}`);
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Server Started Successfully on Port : ${ServerConfig.PORT}`);
+  Logger.info("Message Coming", {} )
 });
